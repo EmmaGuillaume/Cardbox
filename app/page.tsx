33815cf0d57ccf8e5653f251be49bc371d61a2b7
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2026-04-10 12:35:13 +0200

    feat : create Item Search for search futur bar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import FilmCard from "@/components/features/FilmCard";
+import ItemSearch from "@/components/features/ItemSearch";
 import ButtonInterract from "@/components/ui/ButtonInterract";
 import Image from "next/image";
 import { useState } from "react";
@@ -16,6 +17,22 @@ export default function Home() {
         isInWatchlist={true}
         isSeen={true}
       ></FilmCard>
+
+      <div className="flex flex-col w-1/2 bg-background-800/85 gap-2 p-2 rounded-md">
+        <ItemSearch
+          type="film"
+          title="In the mood for love"
+          filmDirector="Wong Kar-wai"
+          filmImageURL="https://i.pinimg.com/736x/60/f5/26/60f526f8b6eef36c5dc933c706ae2b7c.jpg"
+        />
+
+        <ItemSearch
+          type="human"
+          humanRole="Actor"
+          humanName="Tony Leung"
+          humanImageURL="https://media.gq.com/photos/612be4ed73b9651b2559a70e/master/pass/tony-leung-gq-october-2021-07.jpg"
+        />
+      </div>
     </div>
   );
 }
---
+++ components/features/ItemSearch.tsx
@@ -0,0 +1,81 @@
+import ButtonInterract from "../ui/ButtonInterract";
+
+type Props = {
+  type: "film" | "human" | "serie";
+  title?: string;
+  filmDirector?: string;
+  filmImageURL?: string;
+  humanRole?: string;
+  humanName?: string;
+  humanImageURL?: string;
+};
+
+const ItemSearch = ({
+  type,
+  title,
+  filmDirector,
+  humanName,
+  filmImageURL,
+  humanRole,
+  humanImageURL,
+}: Props) => {
+  if (type === "film" || type === "serie") {
+    return (
+      <div className="bg-background w-full rounded-md flex flex-wrap justify-center md:justify-between items-center p-2 text-primary">
+        <div className="flex items-center gap-4">
+          <img src={filmImageURL} alt={title} className="w-12 rounded-sm" />
+          <div>
+            <h2 className="md:text-lg font-krub font-light">{title}</h2>
+            <div className="flex flex-nowrap gap-2 opacity-80">
+              <p>{type === "film" ? "Film" : "Série"}</p>
+              <p>•</p>
+              <p>{filmDirector}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-4 items-center mt-2 md:mt-0">
+          <ButtonInterract
+            isAlreadyAdded={false}
+            type="list"
+            onClick={() => console.log("Marked as seen")}
+          />
+          <ButtonInterract
+            isAlreadyAdded={false}
+            type="like"
+            onClick={() => console.log("Liked")}
+          />
+          <ButtonInterract
+            isAlreadyAdded={false}
+            type="watchlist"
+            onClick={() => console.log("Added to watchlist")}
+          />
+        </div>
+      </div>
+    );
+  } else if (type === "human") {
+    return (
+      <div className="bg-background w-full rounded-md flex flex-wrap justify-center md:justify-between items-center p-2 text-primary">
+        <div className="flex items-center gap-4">
+          <img src={humanImageURL} alt={humanName} className="w-12 rounded-sm" />
+          <div>
+            <h2 className="md:text-lg font-krub font-light">{humanName}</h2>
+            <div className="flex flex-nowrap gap-2 opacity-80">
+              <p>{humanRole}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-4 items-center mt-2 md:mt-0">
+          <ButtonInterract
+            isAlreadyAdded={false}
+            type="like"
+            onClick={() => console.log("Liked")}
+          />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ItemSearch;
