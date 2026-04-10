269fd44ca52d68ffa01de19c2cd68c9c32fc1637
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2026-04-10 11:26:47 +0200

    feat : end component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,21 @@
 "use client";
+import FilmCard from "@/components/features/FilmCard";
 import ButtonInterract from "@/components/ui/ButtonInterract";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-  const [isClicked, setIsClicked] = useState(false);
   return (
     <div className="bg-background min-h-screen flex flex-col items-center justify-center">
-      <ButtonInterract
-        onClick={() => setIsClicked(!isClicked)}
-        type="like"
-        isAlreadyAdded={isClicked}
-      />
-      <ButtonInterract
-        onClick={() => setIsClicked(!isClicked)}
-        type="watchlist"
-        isAlreadyAdded={isClicked}
-      />
-
-      <ButtonInterract
-        onClick={() => setIsClicked(!isClicked)}
-        type="list"
-        isAlreadyAdded={isClicked}
-      />
+      <FilmCard
+        title="In the mood for love"
+        realisateur="Wong Kar-wai"
+        date="2000"
+        imageURL="https://i.pinimg.com/736x/60/f5/26/60f526f8b6eef36c5dc933c706ae2b7c.jpg"
+        isLiked={true}
+        isInWatchlist={true}
+        isSeen={true}
+      ></FilmCard>
     </div>
   );
 }
