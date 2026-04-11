a0105318d0b6cbef1988e23e2528bf063e66f78d
clemtomera <clement.tomera.smb@gmail.com>
2026-04-11 20:17:30 +0200

    feat: integrate TapBar component and create profile activity pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,29 @@
 import ButtonInterract from "@/components/ui/ButtonInterract";
 import Image from "next/image";
 import { useState } from "react";
-import TapBar from "@/components/ui/TapBar";
 
 export default function Home() {
   const [isClicked, setIsClicked] = useState(false);
   return (
-    <div className="bg-background min-h-screen flex flex-col items-center justify-center">
-      <ButtonInterract
-        onClick={() => setIsClicked(!isClicked)}
-        type="like"
-        isAlreadyAdded={isClicked}
-      />
-      <ButtonInterract
-        onClick={() => setIsClicked(!isClicked)}
-        type="watchlist"
-        isAlreadyAdded={isClicked}
-      />
-
-      <ButtonInterract
-        onClick={() => setIsClicked(!isClicked)}
-        type="list"
-        isAlreadyAdded={isClicked}
-      />
-      <TapBar></TapBar>
-    </div>
+    <>
+      <h1>Accueil</h1>
+      <div className="bg-background min-h-screen flex flex-col items-center justify-center">
+        <ButtonInterract
+          onClick={() => setIsClicked(!isClicked)}
+          type="like"
+          isAlreadyAdded={isClicked}
+        />
+        <ButtonInterract
+          onClick={() => setIsClicked(!isClicked)}
+          type="watchlist"
+          isAlreadyAdded={isClicked}
+        />
+        <ButtonInterract
+          onClick={() => setIsClicked(!isClicked)}
+          type="list"
+          isAlreadyAdded={isClicked}
+        />
+      </div>
+    </>
   );
 }
--- components/ui/TapBar.tsx
+++ components/ui/TapBar.tsx
@@ -1,25 +1,26 @@
 "use client";
+import Link from 'next/link'
 import { House, Search, Library, Activity} from "lucide-react";
 
 export default function TapBar() {
     return (
         <div className="w-full h-16 px-4 bg-background-900 flex justify-around items-center fixed bottom-0 border-t border-background-800 backdrop-blur-lg bg-opacity-80 text-primary font-krub text-xs lg:hidden">
-            <button className="flex flex-col items-center cursor-pointer">
+            <Link href="/" className="flex flex-col items-center cursor-pointer">
                 <House className="w-5 h-5" />
                 <span>Accueil</span>
-            </button>
-            <button className="flex flex-col items-center cursor-pointer">
+            </Link>
+            <Link href="/search" className="flex flex-col items-center cursor-pointer">
                 <Search className="w-5 h-5" />
                 <span>Rechercher</span>
-            </button>
-            <button className="flex flex-col items-center cursor-pointer">
+            </Link>
+            <Link href="/profile/1/list" className="flex flex-col items-center cursor-pointer">
                 <Library className="w-5 h-5" />
                 <span>Bibliothèque</span>
-            </button>
-            <button className="flex flex-col items-center cursor-pointer">
+            </Link>
+            <Link href="/profile/1/activity" className="flex flex-col items-center cursor-pointer">
                 <Activity className="w-5 h-5" />
                 <span>Activité</span>
-            </button>
+            </Link>
         </div>
     );
 }
